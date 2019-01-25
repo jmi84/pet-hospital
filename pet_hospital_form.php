e583b9740c74b56fe3e082aafd390ee96f05fd13
jiguchi <jamie.iguchi@gmail.com>
2019-01-24 23:43:42 -0700

    Cleanup. Moved list of names to bottom of the page.

--- pet_hospital_form.php
+++ pet_hospital_form.php
@@ -2,63 +2,38 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>New Pets</title>
   </head>
+
   <body>
 
     <style>
     </style>
 
-
     <?php
 
-    $user = 'root';
-    $password = 'root';
-    $db = 'pet_hospital';
-    $host = 'localhost';
-    $port = 8889;
+      $user = 'root';
+      $password = 'root';
+      $db = 'pet_hospital';
+      $host = 'localhost';
+      $port = 8889;
 
-    $link  = mysqli_init();
-    $success =  mysqli_real_connect(
-      $link,
-      $host,
-      $user,
-      $password,
-      $db,
-      $port
-    );
+      $link  = mysqli_init();
+      $success =  mysqli_real_connect(
+        $link,
+        $host,
+        $user,
+        $password,
+        $db,
+        $port
+      );
 
+      echo '<h1>ENTER NEW PET RECORD</h1>';
 
-    // retrieving pet
-    $sql = "SELECT * FROM Animals";
-
-
-    if ($result = $link->query($sql)) {
-      printf("Select returned %d rows.\n", $result->num_rows);
-      echo '<pre>';
-      print_r($result->fetch_all(MYSQLI_ASSOC));
-      echo '</pre>';
-      $result->close();
-    }
-
-
-      echo '<h1>PET DATABASE MANIPULATION FORM</h1>';
-
-      // inserting data into DATABASE
-
-
-      $hospital_name = "Hospital: ".htmlspecialchars($_POST['hospital_name']);
-      $pet_name = "Pet Name: ".htmlspecialchars($_POST['pet_name']);
-      $owner_name = "Owner Name: ".htmlspecialchars($_POST['owner_name']);
-      $malady = "Reason for Visit: ".htmlspecialchars($_POST['maladies']);
-
-      // function assemblePage(){
-      //   $html =
-      //     '<h3>'.$GLOBALS[$sql].'</h3>';
-      //
-      //   return $html;
-      // }
-      //echo assemblePage();
+      // $hospital_name = "Hospital: ".htmlspecialchars($_POST['hospital_name']);
+      // $pet_name = "Pet Name: ".htmlspecialchars($_POST['pet_name']);
+      // $owner_name = "Owner Name: ".htmlspecialchars($_POST['owner_name']);
+      // $malady = "Reason for Visit: ".htmlspecialchars($_POST['maladies']);
 
     ?>
 
@@ -69,10 +44,23 @@
       <input type="text" name="malady" placeholder="Malady" /><br>
       <button type ="submit">Submit</button>
     </form>
+    <br>
+
+    <?php
+
+      // displaying all Animal records on page
+      $sql = "SELECT * FROM Animals";
+      if ($result = $link->query($sql)) {
+        printf("There are currently %d pets in the database: \n", $result->num_rows);
+        echo '<pre>';
+        print_r($result->fetch_all(MYSQLI_ASSOC));
+        echo '</pre>';
+        $result->close();
+      }
+
+    ?>
 
 <!-- note to self: make separate page for GET -->
-
-
 
   </body>
 </html>
